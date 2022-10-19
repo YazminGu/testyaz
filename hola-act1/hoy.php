d0504d51a97c9a206b7c1eadd494b28ce2c776fa
MarlenDp <114587699+MarlenDp@users.noreply.github.com>
2022-10-18 19:38:34 -0500

    Hola

---
+++ hola-act1/hoy.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/estilos.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Rubik+Dirt&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+    echo '<h3>';
+    echo date('d-m-y');
+    echo '</h3>'
+    ?>
+</body>
+
+</html>
---
+++ java -act-5/greeting.php
@@ -0,0 +1,8 @@
+<?php
+    $nombre = $_GET ['name'];
+
+    echo "<h1>Hola $nombre</h1>";
+    echo "<h1>Tu edad es: $old</h1";
+    echo "<h1>Tu numero de telefono es: $telephone</h1>"
+    echo "<hr>";
+    echo "<a href='../html/greetingForm.html'>Volver</a>";
